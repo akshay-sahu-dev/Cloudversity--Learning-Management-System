86719d3fb6a7a6c938f910c245c785fa445685f8
Akshay Sahu <akki5233@gmail.com>
2021-06-06 22:22:06 +0530

    Setting up express server

---
+++ server/main.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const PORT = process.env.PORT || 5233;
+const cors = require('cors');
+const MongoInit = require('./Config/Mongo');
+const app = express();
+
+//-------- Connecting to DB --------- //
+MongoInit()
+//-------- Setting Up static folder --------- //
+app.use(express.static('public'))
+//-------- Setting up Body Parser --------- //
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit:'50mb', extended:true}));
+//-------- Using CORS middleware --------- //
+app.use(cors());
+
+//-------- Route to test Sever Health --------- //
+app.get('/health-check', (req, res)=> {
+    res.send("Server is healthy, Health Check Passed!")
+})
+
+
+//-------- Listening to the PORT --------- //
+app.listen(PORT, ()=> {
+    console.log(`Listening to http://localhost:${PORT}`)
+})
