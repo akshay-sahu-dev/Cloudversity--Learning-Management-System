1c1ce9ef25cd3c4ab32d7b20a9751ad249eb8c83
Akshay Sahu <akki5233@gmail.com>
2021-06-07 02:01:17 +0530

    Added Mongoose models for Student and tutor

---
+++ Model/student.js
@@ -0,0 +1,33 @@
+const  mongoose = require('mongoose');
+
+const StudentSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    role: {
+        type:String,
+        required: true,
+        default: 'student'
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.Model('student', StudentSchema);
---
+++ Routes/user.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const Router = express.Router();
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+
+
+
+module.exports = Router;
