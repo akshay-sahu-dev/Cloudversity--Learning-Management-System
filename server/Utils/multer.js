754644c14696abe87b619fa3670a7ef50162a66a
Akshay Sahu <akki5233@gmail.com>
2021-06-09 23:55:06 +0530

    Image upload to cloudinary using course route is successful

---
+++ server/Utils/multer.js
@@ -0,0 +1,44 @@
+const multer = require("multer");
+const path = require('path');
+//Specify the storage engine
+let imageUpload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: function (req, file, callback) {
+        var ext = path.extname(file.originalname);
+        if (ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+            let newError = new Error("Only (.png .jpg .jpeg .gif )Images are allowed");
+            newError.name = "MulterError";
+            callback(newError, false);
+            return
+        };
+        callback(null, true);
+       
+    }
+});
+
+
+let videoUpload = multer({
+    storage: multer.memoryStorage(),
+    limits: {
+        fileSize: 1024 * 1024 * 100
+    },
+    fileFilter: function (req, file, callback) {
+        if (file.mimetype === "video/mp4") {
+            callback(null, true)
+        }
+        else {
+            //prevent the upload
+            var newError = new Error("File type is incorrect");
+            newError.name = "MulterError";
+            callback(newError, false);
+        }
+    }
+});
+
+
+
+
+module.exports = {videoUpload, imageUpload}
